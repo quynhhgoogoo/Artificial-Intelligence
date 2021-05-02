1088ee65261b1d9737ab189aea133336adda16e6
quynhhgoogoo <luongdiemquynh1998@gmail.com>
2021-05-02 23:25:52 +0300

    project0, tictactoe: add basic game functions

--- project0/tictactoe/tictactoe.py
+++ project0/tictactoe/tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -61,29 +62,72 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    pass
+    result_board = copy.deepcopy(board)
+    possible_moves = actions(board)
 
+    # If player made an invalid move
+    if action is not in possible_moves:
+        raise NameError("Invalid action")
+    else:
+        result_board[i][j] = player(board)
+    return result_board
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    pass
+    # Check winner in row
+    for row in range(0,3):
+        if board[row][0] == board[row][1] == board [row][2] =! None:
+            if board[row][0] == X:
+                return X
+            else:
+                return Y
+    
+    # Check winner in column
+    for col in range (0,3):
+        if board[0][col] == board[1][col] == board [2][col] =! None:
+            if board[row][0] == X:
+                return X
+            else:
+                return Y
 
+    # Check winner diagonally
+    if board[0][0] == board[1][1] == board [2][2] =! None:
+        if board[0][0] == X:
+            return X
+        else:
+            return Y
+    
+    if board[0][2] == board[1][1] == board [2][0] =! None:
+        if board[0][0] == X:
+            return X
+        else:
+            return Y
+
+    return None
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    i
-    pass
-
+    if player(board) == None:
+        return True
+    elif winner(board) != None:
+        return True
+    else:
+        return False
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    pass
+    if winner(board) == X:
+        return 1
+    elif winner(board) == O:
+        return -1
+    else:
+        return 0
 
 
 def minimax(board):
