6086e221c31d3e6247131512adf8d75ece7407c0
quynhhgoogoo <luongdiemquynh1998@gmail.com>
2021-05-15 17:09:45 +0300

    project1: knights, finished

--- project1/knights/puzzle.py
+++ project1/knights/puzzle.py
@@ -12,21 +12,54 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    # Game's rule: player cannot be both knight and knave 
+    Or(AKnight, AKnave),
+
+    # If A is a Knight
+    Implication(AKnight, And(AKnight, AKnave)),
+    # Elif A is a knave
+    Implication(AKnave, Not(And(AKnight, AKnave)))
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    # Game's rule
+    Or(AKnight, AKnave),
+    Or(BKnight, AKnave),
+
+    Implication(AKnight, And(AKnave, BKnave)),
+    Implication(AKnave, Not(And(AKnave, BKnave)))
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    # Game's rule
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+
+    Implication(AKnight, Or(
+        And(AKnight, BKnight),
+        And(AKnave, BKnave)
+    )),
+
+    Implication(AKnave, Not(Or(
+        And(AKnight, BKnight),
+        And(AKnave, BKnave))
+    )),
+
+    Implication(BKnight, Or(
+        And(AKnight, BKnave),
+        And(AKnave, BKnight)
+    )),
+    
+    Implication(BKnave, Not(Or(
+        And(AKnight, BKnave),
+        And(AKnave, BKnight))
+    ))
 )
 
 # Puzzle 3
@@ -35,7 +68,28 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    # Game's rule
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Or(CKnight, CKnave),
+
+    Implication(AKnight, Or(AKnight, AKnave)),
+    Implication(AKnave, Not(Or(AKnave, AKnight))),
+
+    Implication(BKnight, And( 
+        Implication(AKnight, AKnave),
+        Implication(AKnave, AKnight)
+    )),
+    Implication(BKnave, Not(And(
+        Implication(AKnight, AKnave),
+        Implication(AKnave, AKnight)
+    ))),
+    
+    Implication(BKnight, CKnave),
+    Implication(BKnave, CKnight),
+
+    Implication(CKnight, AKnight),
+    Implication(CKnave, AKnave)
 )
 
 
