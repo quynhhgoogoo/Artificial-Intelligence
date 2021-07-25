ce15560d2c6a29ed82e6773a7d76e0bee9cc6ec0
quynhhgoogoo <luongdiemquynh1998@gmail.com>
2021-07-25 11:49:43 +0300

    week3, lec3.1, update hill climbing algo

--- lectures/hospital/hospitals.py
+++ lectures/hospital/hospitals.py
@@ -13,27 +13,128 @@ class Space():
 
     def add_house(self, row, col):
         ''' Add a house at a specific location in state space '''
-        self.house.add((row, col))
-
+        self.houses.add((row, col))
+    
     def available_spaces(self):
         '''Returns all available cells which is not used by a house or hospital'''
-        pass
+        # Consider all posible cells
+        candidates = set (
+            (row, col)
+            for row in range (self.height)
+            for col in range (self.width)
+        )
+
+        #Remove all cells used by houses and hospitals
+        for house in self.houses:
+            candidates.remove(house)
+        for hospital in self.hospitals:
+            candidates.remove(hospital)
+
+        return candidates
 
     def hill_climb(self, maximum = None, image_prefix = None, log = False):
         '''Perform Hill Climbing to find solution'''
-        pass
+        count = 0
 
+        # Initialize hospitals randomly
+        self.hospitals = set()
+        for i in range (self.num_hospitals):
+            self.hospitals.add (random.choice(list(self.available_spaces())))
+        if log:
+            print("Initial State: cost", self.get_cost(self.hospitals))
+        if image_prefix:
+            self.output_image(f"{image_prefix}{str(count).zfill(3)}.png")
+
+        # Continue until we reach to max number of iterations
+        while maximum is None or count < maximum:
+            count += 1
+            best_neighbors = []
+            best_neighbors_cost = None
+
+            for hospital in self.hospitals:
+                for replacement in self.get_neighbors(*hospital):
+                    # Replace the old hospital placement with new one inside neigbour list
+                    neighbour = self.hospitals.copy()
+                    neighbour.remove(hospital)
+                    neighbour.add(replacement)
+
+                    # Check if neighbour is the best one so far
+                    cost = self.get_cost(neighbour)
+                    
+                    if best_neighbors_cost is None or cost < best_neighbors_cost:
+                        # Overwrite best neighbour list and update new cost
+                        best_neighbors_cost = cost
+                        best_neighbors = [neighbour]
+
+                    elif best_neighbors_cost == cost:
+                        # Add new neighbour to the list
+                        best_neighbors.append(neighbour)
+
+            # If none of the neigbours are better than current state
+            if best_neighbors_cost >= self.get_cost(self.hospitals):
+                return self.hospitals
+            else:
+                # Move to highest-valued neighbour
+                if log:
+                    print(f"Found better neighbor: cost {best_neighbors_cost}")
+                self.hospitals = random.choice(best_neighbors)
+            
+            if image_prefix:
+                self.output_image(f"{image_prefix}{str(count).zfill(3)}.png")
+            
     def random_restart(self, maximum, image_prefix=None, log=False):
         '''Repeats hill-climbing multiple times.'''
-        pass
+        best_hospitals = None
+        best_cost = None
+
+        # Repeat hill-climbing a fixed number of times
+        for i in range (maximum):
+            hospitals = self.hill_climb()
+            cost = self.get_cost(hospitals)
+            
+            if best_cost is None or cost < best_cost:
+                best_cost = cost
+                best_hospitals = hospitals
+                if log:
+                    print(f"{i}: Found new best state: cost {cost}")
+            else:
+                if log:
+                    print(f"{i}: Found state: cost {cost}")
+            
+            if image_prefix:
+                self.output_image(f"{image_prefix}{str(i).zfill(3)}.png")
+        return best_hospitals
+
 
     def get_cost(self, hospitals):
         '''Calculates sum of distances from houses to nearest hospital'''
-        pass
+        cost = 0
+        for house in self.houses:
+            cost += min (
+                abs (house[0] - hospital[0]) + abs(house[1] - hospital[1])
+                for hospital in hospitals
+            )
+        
+        return cost
 
     def get_neighbors(self, row, col):
         '''Returns neighbors not already containing a house or hospital'''
-        pass
+        # Get all the possible neighbour's placement
+        candidates = [
+            (row - 1, col),
+            (row + 1, col),
+            (row, col + 1),
+            (row, col - 1)
+        ]
+        neighbours = []
+
+        for r, c in candidates:
+            # Keep looking for placement are not placed by houses and hospitals
+            if (r, c) in self.houses or (r, c) in self.hospitals:
+                continue
+            if 0 <= r < self.height and 0 <= c <= self.width:
+                neighbours.append((r, c))
+        return neighbours
 
     def output_image(self, filename):
         '''Generates image with all houses and hospitals.'''
@@ -50,13 +151,13 @@ class Space():
              self.height * cell_size + cost_size + padding * 2),
             "white"
         )
-        house = Image.open("assets/images/House.png").resize(
+        house = Image.open("lectures/hospital/assets/images/House.png").resize(
             (cell_size, cell_size)
         )
-        hospital = Image.open("assets/images/Hospital.png").resize(
+        hospital = Image.open("lectures/hospital/assets/images/Hospital.png").resize(
             (cell_size, cell_size)
         )
-        font = ImageFont.truetype("assets/fonts/OpenSans-Regular.ttf", 30)
+        font = ImageFont.truetype("lectures/hospital/assets/fonts/OpenSans-Regular.ttf", 30)
         draw = ImageDraw.Draw(img)
  
         for i in range(self.height):
