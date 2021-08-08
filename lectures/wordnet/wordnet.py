ef9850aca76361b3937af4b05a67b11600d4756f
quynhhgoogoo <luongdiemquynh1998@gmail.com>
2021-08-08 16:58:41 +0300

    week6, lecture notes

---
+++ lectures/wordnet/wordnet.py
@@ -0,0 +1,11 @@
+import nltk
+from nltk.corpus import wordnet
+
+word = input("Word: ")
+synsets = wordnet.synsets(word)
+
+for synset in synsets:
+    print()
+    print(f"{synset.name()}: {synset.definition()}")
+    for hypernym in synset.hypernyms():
+        print(f"  {hypernym.name()}, {hypernym.definition()}")
